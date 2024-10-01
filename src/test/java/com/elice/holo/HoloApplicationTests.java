4b352e7f903cb0a42cb37c83ffd23b1cdfb7b01a
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-01 13:27:38 +0900

    Initial commit

---
+++ src/test/java/com/elice/holo/HoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.holo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
