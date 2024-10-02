6e518988ad1a99caaf0b6d9d03f2045f97fb2b32
Byesol <bsj5967@naver.com>
2024-10-02 17:53:09 +0900

    Refactor: Member responseDto 생성자 수정

--- src/main/java/com/elice/holo/member/dto/MemberResponseDto.java
+++ src/main/java/com/elice/holo/member/dto/MemberResponseDto.java
@@ -1,7 +1,10 @@
 package com.elice.holo.member.dto;
 
+import com.elice.holo.member.domain.Member;
 import lombok.Getter;
 import lombok.Setter;
+
+
 
 @Getter
 @Setter
@@ -13,4 +16,15 @@ public class MemberResponseDto {
     private Boolean gender;
     private Integer age;
     private Boolean isAdmin;
+
+    // Member 객체를 받아 필드를 초기화하는 생성자 추가
+    public MemberResponseDto(Member member) {
+        this.memberId = member.getMemberId();
+        this.email = member.getEmail();
+        this.name = member.getName();
+        this.tel = member.getTel();
+        this.gender = member.getGender();
+        this.age = member.getAge();
+        this.isAdmin = member.getIsAdmin();
+    }
 }
