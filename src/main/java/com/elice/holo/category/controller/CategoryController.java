24980060084c33779c7c07d131781891cd096150
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-10-02 17:58:36 +0900

    feat: Category Delete api 추가

--- src/main/java/com/elice/holo/category/controller/CategoryController.java
+++ src/main/java/com/elice/holo/category/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.elice.holo.category.dto.CategoryResponseDto;
 import com.elice.holo.category.service.CategoryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,11 @@ public class CategoryController {
     @GetMapping("/about/{id}")
     public CategoryDetailsDto getCategoryDetails(@PathVariable(name = "id") Long id) {
         return categoryService.getCategoryById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable(name = "id") Long id) {
+        categoryService.deleteCategory(id);
     }
 
 }
