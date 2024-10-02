7a686fb66b0d3f33c888f47745a2ee0dd254cbcc
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-03 01:58:08 +0900

    Test: 상품 등록 테스트 구현

---
+++ src/main/java/com/elice/holo/product/controller/dto/ProductResponseDto.java
@@ -0,0 +1,29 @@
+package com.elice.holo.product.controller.dto;
+
+import com.elice.holo.product.domain.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+public class ProductResponseDto {
+
+    private Long id;
+    private String name;
+    private int price;
+    private String description;
+
+//    private List<ProductImageDto>  TODO 상품 이미지 반환
+
+    public ProductResponseDto(Product product) {
+        new ProductResponseDto(
+            product.getId(), product.getName(), product.getPrice(), product.getDescription()
+        );
+    }
+
+}
