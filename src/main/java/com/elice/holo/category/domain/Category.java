bd3e3bc3c54c17167de90d505b40e8d47270e56a
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-10-02 17:04:55 +0900

    feat: Category Entity에 isDeleted 필드 추가

--- src/main/java/com/elice/holo/category/domain/Category.java
+++ src/main/java/com/elice/holo/category/domain/Category.java
@@ -1,12 +1,20 @@
 package com.elice.holo.category.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Data;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Data
@@ -17,17 +25,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 255)
     private String name;
 
-    @Column(nullable = true, length = 256)
+    @Column(nullable = true, length = 255)
     private String description;
+
+    @Column(nullable = false, length = 1)
+    private Character isDeleted;
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name="modified_at", nullable = false)
+    @Column(name = "modified_at", nullable = false)
     private LocalDateTime modifiedAt;
 
     @ManyToOne
