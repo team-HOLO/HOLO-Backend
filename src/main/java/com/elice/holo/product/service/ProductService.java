97fcaa0aeaaa4d50c525ed625cea4e3fef13f229
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-03 18:20:40 +0900

    Feat: 상품 단일 조회 Service 구현

--- src/main/java/com/elice/holo/product/service/ProductService.java
+++ src/main/java/com/elice/holo/product/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.elice.holo.product.service;
 
 import com.elice.holo.product.domain.Product;
+import com.elice.holo.product.exception.ProductNotFoundException;
 import com.elice.holo.product.repository.ProductRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,18 @@ public class ProductService {
     public Product saveProduct(Product product) {
         return productRepository.save(product);
     }
+
+    /**
+     * 상품 단일 조회
+     *
+     * @param id
+     * @return Product
+     */
+    public Product findProductById(Long id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new ProductNotFoundException("상품이 존재하지 않습니다."));
+    }
+
 
 
 }
