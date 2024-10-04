010d8d53568f75c484f647c2d0627cb9ff384686
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-04 20:08:20 +0900

    Feat: BaseEntity 구현

---
+++ src/main/java/com/elice/holo/common/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.elice.holo.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+
+}
