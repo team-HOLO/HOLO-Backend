5244a49bcbf16405bce72cce77d392f033d148b2
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-07 23:58:11 +0900

    Feat: 상품 상세 조회 쿼리 작성

--- src/main/java/com/elice/holo/product/repository/ProductRepository.java
+++ src/main/java/com/elice/holo/product/repository/ProductRepository.java
@@ -1,8 +1,17 @@
 package com.elice.holo.product.repository;
 
 import com.elice.holo.product.domain.Product;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //상품 상세 화면 쿼리
+    @Query("select p from Product p join fetch p.productOptions po"
+        + " where p.isDeleted = false and po.isDeleted=false and p.productId = :id")
+    Optional<Product> findProductDetailByProductId(Long id);
+
+
 
 }
