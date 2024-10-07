b34eab479a8f8ba3d56c268f99e0896c3b0b2c1e
[CLOUD4] 임서현 <violethart123@gmail.com>
2024-10-07 10:06:08 +0900

    장바구니 서비스 @Transactional 수정

--- src/main/java/com/elice/holo/cart/Service/CartService.java
+++ src/main/java/com/elice/holo/cart/Service/CartService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -28,36 +29,42 @@ public class CartService {
     }
 
     //장바구니 생성
+    @Transactional
     public Cart createCart(Member member){
         Cart cart = Cart.createCart(member);
         return cartRepository.save(cart);
     }
 
     //장바구니에 상품 추가
+    @Transactional
     public void addProductToCart(Cart cart, Product product, Long quantity) {
         cart.addCartPoduct(product,quantity);
         cartRepository.save(cart);
     }
 
     //장바구니에서 상품 제거
+    @Transactional
     public void removeProductFromCart(Cart cart, CartProduct cartProduct) {
         cart.removeCartProduct(cartProduct);
         cartRepository.save(cart);
     }
 
     //상품 수량 업데이트
+    @Transactional
     public void updateProductQuantity(Cart cart, CartProduct cartProduct, Long quantity) {
         cart.updateCartProductQuantity(cartProduct, quantity);
         cartRepository.save(cart);
     }
 
     //장바구니 전체 비우기
+    @Transactional
     public void clearCart(Cart cart) {
         cart.getCartProducts().clear();
         cartRepository.save(cart);
     }
 
     //특정 상품 삭제
+    @Transactional
     public void removeSpecificProducts(Cart cart, List<CartProduct> productsToRemove) {
         for (CartProduct cartProduct : productsToRemove) {
             cart.removeCartProduct(cartProduct);
