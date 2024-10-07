98fec8e5f434a70f233a305bdeaea5effce7b158
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-10-08 01:43:00 +0900

    fix: Category 관련 DTO에서 Setter 어노테이션 제거

--- src/main/java/com/elice/holo/category/dto/CategoryDto.java
+++ src/main/java/com/elice/holo/category/dto/CategoryDto.java
@@ -1,12 +1,12 @@
 package com.elice.holo.category.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class CategoryDto {
+
     private Long categoryId;
     private String name;
-
 }
