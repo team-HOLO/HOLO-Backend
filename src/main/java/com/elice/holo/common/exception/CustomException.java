1d18bd463b057f294ac96a9ff7174974a2bbb5bd
[CLOUD4] 윤지현 <jihyeon.yun619@gmail.com>
2024-10-08 14:46:33 +0900

    feat: 생성자에서 ErrorCode 사용하도록 변경

--- src/main/java/com/elice/holo/common/exception/CustomException.java
+++ src/main/java/com/elice/holo/common/exception/CustomException.java
@@ -6,7 +6,13 @@ public class CustomException extends RuntimeException {
         super(message);
     }
 
-    public CustomException(String message, Throwable cause) {
-        super(message, cause);
+    // 기본 ErrorCode만 사용하는 생성자
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+    }
+
+    // error code 사용 및 메세지 설정 가능
+    public CustomException(ErrorCode errorCode, String detailMessage) {
+        super(String.format("%s - %s", errorCode.getMessage(), detailMessage));
     }
 }
