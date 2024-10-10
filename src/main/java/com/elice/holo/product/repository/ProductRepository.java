0c365216810910a1320ead8da1b91c3413178e95
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-10 14:51:46 +0900

    Feat: QueryDsl 상품 목록 조회 기능 추가

--- src/main/java/com/elice/holo/product/repository/ProductRepository.java
+++ src/main/java/com/elice/holo/product/repository/ProductRepository.java
@@ -1,12 +1,13 @@
 package com.elice.holo.product.repository;
 
 import com.elice.holo.product.domain.Product;
+import com.elice.holo.product.repository.query.ProductRepositoryCustom;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
     //상품 상세 화면 쿼리
     @Query("select p from Product p join fetch p.productOptions po"
