20104637175585c6613167aafa8134a60b267b25
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-18 02:55:28 +0900

    Feat #33 카테고리별 상품 조회 쿼리 작성

--- src/main/java/com/elice/holo/product/repository/query/ProductRepositoryCustom.java
+++ src/main/java/com/elice/holo/product/repository/query/ProductRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.elice.holo.product.repository.query;
 import com.elice.holo.product.domain.Product;
 import com.elice.holo.product.dto.ProductSearchCond;
 import com.elice.holo.product.dto.ProductsResponseDto;
+import com.elice.holo.product.dto.SortBy;
 import com.elice.holo.product.repository.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,4 +15,9 @@ public interface ProductRepositoryCustom {
 
     //관리자 페이지용 쿼리
     Page<Product> findAdminPage(Pageable pageable);
+
+
+    //카테고리별 목록
+    Page<ProductsResponseDto> findCategoryProductsPage(Pageable pageable,
+        ProductSearchCond productSearchCond, Long categoryId, SortBy sort);
 }
