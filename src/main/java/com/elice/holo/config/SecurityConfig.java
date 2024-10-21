b67b8290055b6d14ef2dd83698018145e27348cc
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-22 00:39:37 +0900

    refactor: #50 - 로그인 여부, 관리자 여부 인증 해제

--- src/main/java/com/elice/holo/config/SecurityConfig.java
+++ src/main/java/com/elice/holo/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS)) // 세션을 Stateless로 설정
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/members/signup", "/api/members/login")
+                .requestMatchers("/api/members/signup", "/api/members/login","/api/members/check-admin","/api/members/check-login")
                 .permitAll() // 회원 가입, 로그인 API는 인증 없이 접근 가능
                 .requestMatchers("/api/members/**").authenticated() // 그 외 /api/members 경로는 인증 필요
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
