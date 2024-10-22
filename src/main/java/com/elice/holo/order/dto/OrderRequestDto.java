1c25220441722c62b38f799ffeba8dd10c71d112
[CLOUD4] 손병훈 <son000110@naver.com>
2024-10-22 15:16:46 +0900

    feat: 배송받는 사람과 배송 요청 사항 필드 추가

--- src/main/java/com/elice/holo/order/dto/OrderRequestDto.java
+++ src/main/java/com/elice/holo/order/dto/OrderRequestDto.java
@@ -1,11 +1,15 @@
 package com.elice.holo.order.dto;
 
-import lombok.Data;
-
+import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class OrderRequestDto {
-    private List<OrderProductRequestDto> products;
+
+    private List<OrderProductRequestDto> products = new ArrayList<>();
     private String shippingAddress;
+    private String recipientName;
+    private String shippingRequest;
+
 }
