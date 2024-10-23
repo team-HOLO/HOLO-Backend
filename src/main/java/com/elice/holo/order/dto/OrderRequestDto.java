9845170bbcecb83659818587ee42c537abe37a83
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-24 01:34:29 +0900

    test: #62 OrderServiceTest  구현

--- src/main/java/com/elice/holo/order/dto/OrderRequestDto.java
+++ src/main/java/com/elice/holo/order/dto/OrderRequestDto.java
@@ -2,9 +2,11 @@ package com.elice.holo.order.dto;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class OrderRequestDto {
 
     private List<OrderProductRequestDto> products = new ArrayList<>();
