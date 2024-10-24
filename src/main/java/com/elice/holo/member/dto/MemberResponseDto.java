e1cf03e5269612407e146db28f408656dad2776b
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-25 00:49:01 +0900

    fix: #70 멤버 response dto 수정

--- src/main/java/com/elice/holo/member/dto/MemberResponseDto.java
+++ src/main/java/com/elice/holo/member/dto/MemberResponseDto.java
@@ -14,11 +14,13 @@ public class MemberResponseDto {
 
     private Long memberId;
     private String email;
+
     private String name;
     private String tel;
     private Boolean gender;
     private Integer age;
     private Boolean isAdmin;
+    private String password;
 
     public MemberResponseDto(Member member) {
         this.memberId = member.getMemberId();
@@ -28,5 +30,6 @@ public class MemberResponseDto {
         this.gender = member.getGender();
 //        this.age = member.getAge();
         this.isAdmin = member.getIsAdmin();
+        this.password=member.getPassword();
     }
 }
